0b2491f90bbe064b746a672f6712172d14063647
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 13:44:04 +0530

    [Rakshit] Add: Gambler program completed

---
+++ LogicalProblems/Gambler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class Gambler
+    {
+        public void GamblerSimulation()
+        {
+            Console.WriteLine("Enter the number of times you want to run the experiment");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Stake");
+            int stake = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Goal");
+            int goal = Convert.ToInt32(Console.ReadLine());
+
+            int stakeRemaining = stake;
+            int betWinCount = 0; 
+            bool didWin = false; 
+            int noOfTimesRun = 0;
+
+
+            while (num > 0 || stakeRemaining != 0 || didWin)
+            {
+                Random random = new Random();
+                int RESULT = random.Next(0, 2); 
+
+                stakeRemaining--; 
+
+                if (RESULT == 0) 
+                {
+                    stakeRemaining -= 2; 
+                }
+                else 
+                {
+                    betWinCount++;
+                    stakeRemaining += 2;
+                }
+
+                if (stakeRemaining == goal)
+                {
+                    didWin = true;
+                }
+                noOfTimesRun++;
+                num--;
+            }
+            double percentWin = ((double)betWinCount / noOfTimesRun) * 100;
+            double percentLoss = 100 - percentWin;
+
+            Console.WriteLine("The total no of Bet Wins are: " + betWinCount + "\n Percentage Win: " + percentWin + "\n Percentage Loss: " + percentLoss);
+
+        }
+    }
+}
---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter 1: Gambler\n" + "Enter 2: Generate coupons\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Gambler gambler = new Gambler();
+                        gambler.GamblerSimulation();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter correct option\n");
+                        break;
+
+                }
+            }
+        }
+    }
+}
